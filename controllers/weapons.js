df7e6df795c72e9eed45795e496f5dcaf304937f
DVNJNKNS <devonmjenkins3@gmail.com>
2022-03-07 15:53:11 -0500

    full crud implemented on weapons dataset.

--- controllers/weapons.js
+++ controllers/weapons.js
@@ -63,12 +63,49 @@ function deleteWeapon(req, res){
   })
 }
 
+function edit(req, res){
+  Weapon.findById(req.params.id)
+  .then(weapon => {
+    res.render("weapons/edit",{
+      weapon,
+      title: "weapon edit"
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/weapons')
+  })
+}
+
+function update(req, res){
+  console.log('console.log:')
+  Weapon.findById(req.params.id)
+  .then(weapon => {
+    //if (weapon.owner.equals(req.user.profile._id)){
+      weapon.updateOne(req.body, {new: true})
+      .then(() => {
+        res.redirect(`/weapons/${req.params.id}`)
+      })
+    }
+  //}else {
+  //   throw new Error("NOT AUTHORIZED")
+  // }
+  )
+  .catch(err => {
+    console.log(err)
+    res.redirect('/weapons')
+  })
+}
+
+//remember to go back and uncomment those owner/profile id details. this is necessary for future authorization/guest restriction. 
 
 
 export {
   index,
   create,
   show,
+  edit,
   deleteWeapon as delete,
+  update,
 
 }
--- routes/weapons.js
+++ routes/weapons.js
@@ -6,11 +6,16 @@ const router = Router()
 
 //GET localhost3000: /weapons
 router.get('/', weaponsCtrl.index)
-//GET-localhost:3000/:id
+//GET-localhost:3000/weapons/:id
 router.get('/:id', weaponsCtrl.show) 
+// GET localhost:3000/weapons/:id/edit
+router.get('/:id/edit', weaponsCtrl.edit)
 
 //POST localhost:3000/weapons   add is logged in once testing is complete
 router.post('/', weaponsCtrl.create)
+
+//PUT-localhost3000/weapons/:id
+router.put('/:id', weaponsCtrl.update)
 
 //DELETE - localhost:3000/weapons/:id
 router.delete('/:id', weaponsCtrl.delete)
